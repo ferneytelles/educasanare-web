95118bc78f84b799caac9b3df5c5cd60a0d29e37
Ferney <ferney.telles@unillanos.edu.co>
2021-09-29 18:09:29 -0500

    fbbbdnfggffdfhdhdfgfd

--- src/app/modules/forum/category-item/category-item.component.ts
+++ src/app/modules/forum/category-item/category-item.component.ts
@@ -1,29 +1,21 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-forum-item',
-  templateUrl: './forum-item.component.html',
-  styleUrls: ['./forum-item.component.scss']
+  selector: 'app-category-item',
+  templateUrl: './category-item.component.html',
+  styleUrls: ['./category-item.component.scss']
 })
-export class ForumItemComponent implements OnInit {
+export class CategoryItemComponent implements OnInit {
 
-  forum = {
-    own: false,
-    img: 'assets/images/face3.jpg',
-    user: 'afsdfd_35',
-    date: '21/05/2021 15:45',
-    title: 'Titulo del tema 2',
-    slug: 'titulo-del-tema-2',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga atque minus enim nobis ad, cupiditate, dolorem velit quis modi explicabo fugiat magnam odio, quod officia voluptas ducimus. Recusandae, asperiores labore. cupiditate, dolorem velit quis modi explicabo fugiat magnam odio.'
-  };
-
-  answers = [
+  forums = [
     {
       own: false,
       img: 'assets/images/face1.jpg',
       user: 'afsdfd_35',
       date: '21/05/2021 15:45',
+      title: 'Titulo del tema 1',
+      slug: 'titulo-del-tema-1',
       text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga atque minus enim nobis ad, cupiditate, dolorem velit quis modi explicabo fugiat magnam odio, quod officia voluptas ducimus. Recusandae, asperiores labore. cupiditate, dolorem velit quis modi explicabo fugiat magnam odio.'
     },
     {
@@ -31,6 +23,8 @@ export class ForumItemComponent implements OnInit {
       img: 'assets/images/face3.jpg',
       user: 'afsdfd_35',
       date: '21/05/2021 15:45',
+      title: 'Titulo del tema 2',
+      slug: 'titulo-del-tema-2',
       text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga atque minus enim nobis ad, cupiditate, dolorem velit quis modi explicabo fugiat magnam odio, quod officia voluptas ducimus. Recusandae, asperiores labore. cupiditate, dolorem velit quis modi explicabo fugiat magnam odio.'
     },
     {
@@ -38,31 +32,30 @@ export class ForumItemComponent implements OnInit {
       img: 'assets/images/face2.jpg',
       user: 'afsdfd_35',
       date: '21/05/2021 15:45',
+      title: 'Titulo del tema 3',
+      slug: 'titulo-del-tema-3',
       text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga atque minus enim nobis ad, cupiditate, dolorem velit quis modi explicabo fugiat magnam odio, quod officia voluptas ducimus. Recusandae, asperiores labore. cupiditate, dolorem velit quis modi explicabo fugiat magnam odio.'
-    }
+    },
+    {
+      own: false,
+      img: 'assets/images/face1.jpg',
+      user: 'afsdfd_35',
+      date: '21/05/2021 15:45',
+      title: 'Titulo del tema 4',
+      slug: 'titulo-del-tema-4',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga atque minus enim nobis ad, cupiditate, dolorem velit quis modi explicabo fugiat magnam odio, quod officia voluptas ducimus. Recusandae, asperiores labore. cupiditate, dolorem velit quis modi explicabo fugiat magnam odio.'
+    },
   ];
-
-  height = 60;
-  @ViewChild('textArea') text: ElementRef;
 
   constructor(private route: Router) { }
 
   ngOnInit(): void {
-    this.forum.own = this.route.url.includes('2');
   }
 
-  setHeight(): void{
-    if (this.text.nativeElement.scrollHeight > this.height){
-      this.height = this.text.nativeElement.scrollHeight + 2;
-    }
-    // console.log(this.text.nativeElement.scrollHeight);
-    // console.log(this.text.nativeElement.getBoundingClientRect().height);
-    // console.log(this.text);
-  }
-
-  cancelText(): void{
-    this.text.nativeElement.value = '';
-    this.height = 60;
+  navigate(value: string): void{
+    const url = this.route.url;
+    this.route.navigate([url + '/' + value]);
+    window.scroll({top: 0, behavior: 'smooth'});
   }
 
 }
