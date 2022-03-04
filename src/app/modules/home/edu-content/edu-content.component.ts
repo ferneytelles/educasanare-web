14e1236cc2420c838871608d3401088190314492
Ferney <ferney.telles@unillanos.edu.co>
2022-03-04 08:54:23 -0500

    cambios capsulas

--- src/app/modules/home/edu-content/edu-content.component.ts
+++ src/app/modules/home/edu-content/edu-content.component.ts
@@ -6,54 +6,56 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-edu-content',
   templateUrl: './edu-content.component.html',
-  styleUrls: ['./edu-content.component.scss']
+  styleUrls: ['./edu-content.component.scss'],
 })
 export class EduContentComponent implements OnInit {
-
   @Input() section: any;
   pills = [
     {
       img: 'assets/images/cap1.png',
       title: 'Cápsula 1',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.'
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.',
     },
     {
       img: 'assets/images/cap2.png',
       title: 'Cápsula 2',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.'
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.',
     },
     {
       img: 'assets/images/cap3.png',
       title: 'Cápsula 3',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.'
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.',
     },
     {
       img: 'assets/images/cap4.png',
       title: 'Cápsula 4',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.'
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.',
     },
     {
       img: 'assets/images/cap5.png',
       title: 'Cápsula 5',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.'
-    }
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dignissimos doloribus in, aspernatur voluptatem dolore. Reiciendis aliquam ut, repellendus, laborum harum beatae, dolor obcaecati fugit facilis necessitatibus optio odio veritatis.',
+    },
   ];
 
   labels: any;
 
-  constructor(
-    private route: Router,
-    private storage: SessionStorageService
-  ) { }
+  constructor(private route: Router, private storage: SessionStorageService) {}
 
   ngOnInit(): void {
-    this.labels = this.storage.getStorage(SessionStorageService.keyLabels)[PageService.language];
+    this.labels = this.storage.getStorage(SessionStorageService.keyLabels)[
+      PageService.language
+    ];
     // console.log(this.section);
   }
 
-  navigate(slug: string): void{
-    this.route.navigate([`/buscador/${slug}`]);
-    window.scroll({top: 0, behavior: 'smooth'});
+  navigate(post: any): void {
+    // console.log(post);
+    if (post.external_url) {
+      window.open(post.external_url, '_blank');
+    } else {
+      this.route.navigate([`/buscador/${post.slug}`]);
+      window.scroll({ top: 0, behavior: 'smooth' });
+    }
   }
-
 }
